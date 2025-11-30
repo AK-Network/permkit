4e855cb281b9516a2c2dfc072c8b78e13fd7798f
Angelos Koulouris <ak.network.gr@gmail.com>
2025-11-30 09:38:54 +0200

    ✨ feat(sveltekit.guard): hook with sveltekit's load function so thinks we return here can be accessable in `{data}` in layouts | pages.

--- src/lib/sveltekit/guard.ts
+++ src/lib/sveltekit/guard.ts
@@ -3,25 +3,26 @@ import type { RequestEvent } from "@sveltejs/kit";
 import type { PermissionContext } from "../shared/_types.ts";
 
 export type LoadFunction<T> = (event: RequestEvent) => T | Promise<T>;
+export type WhateverLoadReturns = (event: RequestEvent) => ReturnType<LoadFunction<any>>;
 
 /**
  * @param permission {string}
- * @param loadFn 
+ * @param load
  * @param getResource 
  * @returns 
  * 
  * 
  * #### This provides the magic:
  * ```ts
- * export const load = guard("posts.write", async (event) => { ... })
+ * export const load = guard("posts:write", async (event) => { ... })
  * ```
  * Works in any `+page.server.ts` or `+layout.server.ts`.
  */
 export function guard<T>(
   permission: string,
-  loadFn: LoadFunction<T>,
+  load: LoadFunction<T>,
   getResource?: (event: RequestEvent) => any | Promise<any>
-) {
+): WhateverLoadReturns {
   return (async (event: RequestEvent): Promise<T> => {
     const resource = getResource ? await getResource(event) : undefined;
     const allowed = await event.locals.can(permission, resource);
@@ -30,6 +31,6 @@ export function guard<T>(
       throw error(403, `Forbidden: missing permission "${permission}"`);
     }
 
-    return loadFn(event);
+    return load(event);
   });
 }
