d99b72437043c533c43f846d2fc7ff366c215dd5
Angelos Koulouris <ak.network.gr@gmail.com>
2025-11-30 13:45:55 +0200

    null

--- src/lib/shared/pbac.ts
+++ src/lib/shared/pbac.ts
@@ -5,6 +5,5 @@ export function pbacAllows(
   required: PermissionString,
   assigned: PermissionString[]
 ): boolean {
-	console.error("PBAC CHECK:", { required, assigned });
   return assigned.some(p => matchPermission(required, p));
 }
