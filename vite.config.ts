6b2bf1024989e0a83cbb104b42f51d574786e1ef
Angelos Koulouris <ak.network.gr@gmail.com>
2025-11-30 14:58:11 +0200

    🚀 publish(npm): release v0.1.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
@@ -13,19 +12,19 @@ export default defineConfig({
     },
 		expect: { requireAssertions: true },
 		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
+			// {
+			// 	extends: './vite.config.ts',
+			// 	test: {
+			// 		name: 'client',
+			// 		browser: {
+			// 			enabled: true,
+			// 			provider: playwright(),
+			// 			instances: [{ browser: 'chromium', headless: true }]
+			// 		},
+			// 		include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+			// 		exclude: ['src/lib/server/**']
+			// 	}
+			// },
 			{
 				extends: './vite.config.ts',
 				test: {
